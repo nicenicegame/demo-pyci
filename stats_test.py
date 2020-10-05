159ee60b2a756fda67bbb6836b12f1be0431642c
Tatpol Samakpong <nicenicegame@gmail.com>
2020-10-05 15:59:28 +0700

    Fix main in test file

--- stats_test.py
+++ stats_test.py
@@ -8,9 +8,9 @@ class StatsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of some typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_single_value_is_zero(self):
         """variance of a singleton list should be zero"""
@@ -27,7 +27,7 @@ class StatsTest(TestCase):
         self.assertEqual(0.0, stdev([2, 2, 2, 2]))
         self.assertEqual(math.sqrt(2.0), stdev([1, 2, 3, 4, 5]))
 
+
 if __name__ == '__main__':
     import unittest
     unittest.main(verbosity=1)
-
