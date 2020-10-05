42642339ee15f8fb661da34a85bc57417ff11274
Tatpol Samakpong <nicenicegame@gmail.com>
2020-10-05 15:17:07 +0700

    Fix the bug

--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 def variance(data):
     """Returns the population variance of a list (array) of numbers in data.
-    
+
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list (n).
     This is different from the Python library function statistics.variance
@@ -9,7 +9,7 @@ def variance(data):
     Example: variance([1,5]) is ((1-3)**2 + (5-3)**2)/2 = 4.
 
     (These are Google style comments for arguments, returns, and exceptions.)
-    
+
     Args:
         data: list of numbers for which variance will be computed. 
            Must contain at least one element.
@@ -28,13 +28,13 @@ def variance(data):
     4.0
     """
     # some deliberately misformatted code. Use flake8 to fix.
-    n=len(data)  
-    if n==0:
-        raise TypeError( )
+    n = len(data)
+    if n == 0:
+        raise ValueError()
     average = sum(data)/n
-    return sum( [(x-average)**2 for x in data] )/n
+    return sum([(x-average)**2 for x in data])/n
+
 
 def stdev(data):
     """The population standard deviation of a list of data values."""
     return sqrt(variance(data))
-
